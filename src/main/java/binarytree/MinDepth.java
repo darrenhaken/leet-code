5295557d5214add158e1c7a8fe0becd7802d488d
Darren Haken <darrenhaken@thoughtworks.com>
2016-05-17 18:45:49 -0700

    Update min depth

--- src/main/java/binarytree/MinDepth.java
+++ src/main/java/binarytree/MinDepth.java
@@ -3,16 +3,6 @@ package binarytree;
 public class MinDepth {
 
     public int minDepth(TreeNode root) {
-//        if (root == null) {
-//            return 0;
-//        }
-//
-//        if (root.left != null && root.right != null) {
-//            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-//        } else {
-//            return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
-//        }
-
         if (root == null) {
             return 0;
         }
