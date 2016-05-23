7688943b62e404ff310704c0c5e371f9ac2bca62
Darren Haken <darrenhaken@thoughtworks.com>
2016-05-22 19:03:50 -0700

    Add Digits problem complete

--- src/main/java/binarytree/InvertBinaryTree.java
+++ src/main/java/binarytree/InvertBinaryTree.java
@@ -3,6 +3,20 @@ package binarytree;
 
 public class InvertBinaryTree {
 
+    /*
+     Invert a tree from:
+             4
+            /  \
+           2    7
+          / \   /\
+         1   3 6  9
+     to:
+             4
+            /  \
+           7    2
+          / \   /\
+         9   6 3  1
+     */
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
---
+++ src/main/java/numbers/AddDigits.java
@@ -0,0 +1,16 @@
+package numbers;
+
+
+public class AddDigits {
+
+    /*
+        Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+        For example:
+        Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+        https://leetcode.com/problems/add-digits/
+     */
+    public int addDigits(int num) {
+        return 1 + (num - 1) % 9;
+    }
+}
