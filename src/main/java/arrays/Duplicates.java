f0d5ca798c73408f838dde13f8b91354e9f5c099
Darren Haken <darrenhaken@thoughtworks.com>
2016-06-15 13:22:04 -0700

    Implement find duplicates in array

---
+++ src/main/java/arrays/Duplicates.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Duplicates {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
