e36430b04e23a6451ee89722f0b38c33af10be72
Darren Haken <darrenhaken@thoughtworks.com>
2016-06-15 13:23:14 -0700

    Add docs for duplicates problem

--- src/main/java/arrays/Duplicates.java
+++ src/main/java/arrays/Duplicates.java
@@ -3,6 +3,11 @@ package arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+https://leetcode.com/problems/contains-duplicate/
+ */
 public class Duplicates {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
